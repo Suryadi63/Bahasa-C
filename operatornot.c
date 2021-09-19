33509b492a0070eb27c094e8cbc8c698fbab3573
Suryadi63 <imampwsp@gmail.com>
2021-09-19 13:15:31 +0000

    Menambahkan file opereatornot dan operatornot.c

---
+++ operatornot.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  operatornot.c
+ *
+ *    Description:  Operator NOT merupakan operasi yang menghasilkan nilai ingkaran atau
+ *                  (negasi) dari nilai operand yang diberikan.
+ *
+ *        Version:  1.0
+ *        Created:  09/19/2021 01:11:57 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Suryadi (mn), imampwsp@gmail.com
+ *        Company:  -
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(void) {
+    printf("!1 = %d\n", (!1));
+    printf("!0 = %d\n", (!0));
+
+    return 0;
+
+}
