92ac76fd742e21b0b063e872cbfd43ee663f4252
Suryadi63 <imampwsp@gmail.com>
2021-09-24 23:15:23 +0700

    menambahkan file for.c dan for

---
+++ for.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  for.c
+ *
+ *    Description:  Struktur for ini digunakan untuk menuliskan jenis pengulangan yang 
+ *                  banyaknya sudah pasti atau telah diketahui sebelumnya.
+ *
+ *        Version:  1.0
+ *        Created:  09/24/2021 10:55:35 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Suryadi (mn), imampwsp@gmail.com
+ *        Company:  -
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main (void) {
+    for (int j=0; j<0; j++) {
+        printf("Nama saya Suryadi\n");
+    }
+    return 0;
+}
+
